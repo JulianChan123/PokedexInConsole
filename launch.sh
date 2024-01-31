1ec8a34131b675bc974be9456801aa88ab8471f5
JulianChan123 <a20216390@alumnos.uady.mx>
2024-01-31 16:50:14 -0600

    Ya no le voy a mover xd

--- launch.sh
+++ launch.sh
@@ -4,11 +4,12 @@ if [[ -z $1 ]];
 then 
     echo "Please, give me a pokemon name in the arguments"
 else
+    curlResponse=$(curl -s https://pokeapi.co/api/v2/pokemon/$1)
     if [[ $curlResponse = "Not Found" ]];
     then
         echo "Who's that pokemon?"
     else
-        printf "Nombre:%s\nID: %d\nWeight: %d\nHeight: %d\n" \ $(curl -s https://pokeapi.co/api/v2/pokemon/$1 | jq -r '.name, .id, .weight, .height')
+        printf "Nombre:%s\nID: %d\nWeight: %d\nHeight: %d\n" $(echo "$curlResponse" | jq -r '.name, .id, .weight, .height')
     fi
         
 fi
